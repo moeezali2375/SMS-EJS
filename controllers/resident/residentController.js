9ed02313cd23e3bd542f056b239c6b423cbdebe1
moeezali <moeezali2375@gmail.com>
2023-12-01 23:33:21 +0500

    bill payment bug fix + visitor usecase add

--- controllers/resident/residentController.js
+++ controllers/resident/residentController.js
@@ -1,5 +1,6 @@
 const Bill = require("../../models/bill");
 const Payment = require("../../models/payment");
+const Visitor = require("../../models/visitor");
 
 module.exports.home = (req, res) => {
 	res.render("resident/home");
@@ -28,7 +29,7 @@ module.exports.get_bills_unpaid = async (req, res) => {
 module.exports.bill_detail_page = async (req, res) => {
 	try {
 		const bill = await Bill.findById(req.params.id);
-		const payment = await Payment.findById(bill._id);
+		const payment = await Payment.findOne({ billId: bill._id });
 		res.render("resident/billDetail", { bill: bill, payment: payment });
 	} catch (error) {
 		console.log(error);
@@ -36,7 +37,6 @@ module.exports.bill_detail_page = async (req, res) => {
 };
 
 module.exports.pay_bill = async (req, res) => {
-	let message;
 	try {
 		const { billId, bank, tid } = req.body;
 		await Payment.insertMany({
@@ -45,10 +45,66 @@ module.exports.pay_bill = async (req, res) => {
 			tid: tid,
 			date: new Date(),
 		});
-		message = "Payment information uploaded successfully!";
+		req.session.message = "Payment information uploaded successfully!";
 	} catch (error) {
 		console.log(error);
-		message = "Error uploading payment information!";
+		req.session.message = "Error uploading payment information!";
 	}
 	res.redirect("/resident/bills/unpaid");
+};
+
+module.exports.verified_visitors = async (req, res) => {
+	try {
+		const visitors = await Visitor.find({
+			residentId: req.user._id,
+			isVerified: true,
+		});
+		res.render("resident/verifiedVisitors", { visitors: visitors });
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+module.exports.unverified_visitors = async (req, res) => {
+	try {
+		const message = req.session.message;
+		req.session.message = null;
+		const visitors = await Visitor.find({
+			residentId: req.user._id,
+			isVerified: false,
+		});
+		res.render("resident/verifiedVisitors", {
+			visitors: visitors,
+			message: message,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+module.exports.visitor_detail_page = async (req, res) => {
+	try {
+		const visitor = await Visitor.findById(req.params.id);
+		res.render("resident/visitorDetail", { visitor: visitor });
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+module.exports.register_visitor = async (req, res) => {
+	try {
+		const { name, cnic, reason, date } = req.body;
+		await Visitor.insertOne({
+			residentId: req.user._id,
+			name: name,
+			cnic: cnic,
+			reason: reason,
+			date: date,
+		});
+		req.session.message = "Visitor Registered Successfully!";
+	} catch (error) {
+		console.log(error);
+		req.session.message = "Error Verifying Visitor";
+	}
+	res.redirect("/resident/visitors/unverified");
 };
--- routes/resident/residentRoutes.js
+++ routes/resident/residentRoutes.js
@@ -3,9 +3,16 @@ const express = require("express");
 const { residentAuth } = require("../../middlewares/residentAuth");
 const {
 	home,
+	//1
 	get_bills_paid,
 	get_bills_unpaid,
 	bill_detail_page,
+	pay_bill,
+	verified_visitors,
+	unverified_visitors,
+	visitor_detail_page,
+	register_visitor,
+	//2
 } = require("../../controllers/resident/residentController");
 
 const residentRouter = express.Router();
@@ -18,6 +25,14 @@ residentRouter.get("/bills/unpaid", residentAuth, get_bills_unpaid);
 
 residentRouter.get("/bills/:id/details", residentAuth, bill_detail_page);
 
-residentRouter.post("/bills/:id/pay", residentAuth);
+residentRouter.post("/bills/:id/pay", residentAuth, pay_bill);
+
+residentRouter.get("/visitors/verified", residentAuth, verified_visitors);
+
+residentRouter.get("/visitors/unverified", residentAuth, unverified_visitors);
+
+residentRouter.get("/visitors/:id/details", residentAuth, visitor_detail_page);
+
+residentRouter.post("/visitors/register", residentAuth, register_visitor);
 
 module.exports = residentRouter;
